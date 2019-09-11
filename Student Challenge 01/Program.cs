41dd86284aed797aa2312d9c30c46e42547a6ae1
Youssef Shoaib <yhshoaib@students.wcpss.net>
2019-09-11 07:25:38 -0400

    Add project files.

---
+++ Student Challenge 01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Challenge_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string strUserName = Console.ReadLine();
+            Console.WriteLine("Hello " + strUserName);
+            Console.ReadKey();
+        }
+    }
+}
